c85d77d89275d6cb4faacee0416a082603fbed23
Руфат_Ольга <rufattabaev@gmail.com>
2019-09-25 20:44:41 +0300

    fixes #2

--- src/DistanceCalculator.java
+++ src/DistanceCalculator.java
@@ -1,6 +1,6 @@
 public class DistanceCalculator {
-    static double calculateDistance(double fuelVolume, double fuelConsumption){
-        double distance = (double) fuelVolume / fuelConsumption * 100;
+    static int calculateDistance(int fuelVolume, int fuelConsumption){
+        int distance = fuelVolume / fuelConsumption * 100;
         return distance;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Укажите уровень оставшегося топлива");
-        double fuelVolume = scanner.nextDouble();
+        int fuelVolume = (int) scanner.nextDouble();
         System.out.println("Укажите расход топлива");
-        double fuelConsumption = scanner.nextDouble();
+        int fuelConsumption = (int) scanner.nextDouble();
         System.out.println("Оставшееся расстояние = " + DistanceCalculator.calculateDistance(fuelVolume, fuelConsumption));
 
 
